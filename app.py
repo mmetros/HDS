85b1e58b01a1ad1e8cf3191975c7a4402c313af2
Matthew <mmetros@wesleyan.edu>
2018-09-23 21:24:33 -0400

    Getting Everything Ready

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from flask_restful import Api
+
+
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
