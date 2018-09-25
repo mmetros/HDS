e39237056528b845e0289b67b7e25d4ef72f37f4
Matthew <mmetros@wesleyan.edu>
2018-09-25 12:26:54 -0400

    Finished Admin Page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_restful import Api
+import sqlite3
 
 
 
@@ -18,6 +19,54 @@ def manufacturers():
 def admin():
     return render_template('admin.html')
 
+
+@app.route('/_background_process')
+def _background_process():
+    name = request.args.get('name')
+    link = request.args.get('link')
+    logo = request.args.get('logo')
+    if find_by_name(name, link, logo):
+        update(name, link, logo)
+        return jsonify(result = "{} has been updated with the following info: Name: {}, Link: {}, Logo: {}".format(name, name,link,logo))
+    else:
+        if None:
+            return
+        else:
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
+            query = "INSERT INTO manufacturers VALUES (?,?,?)"
+            result = cursor.execute(query, (name, link, logo))
+            connection.commit()
+            connection.close()
+            return jsonify(result = "The following has been added: Name: {}, Link: {}, Logo: {}".format(name,link,logo))
+
+
+def find_by_name(name, link, logo):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM manufacturers WHERE name=?"
+    result = cursor.execute(query, (name,))
+    row = cursor.fetchone()
+    # Row will return a tuple
+
+    connection.close()
+    if row:
+        if row[0] == name and row[1] == link and row[2] == logo:
+            return True
+    else:
+        return False
+
+def update(name, link, logo):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    query = "UPDATE manufacturers SET link=?, logo=? WHERE name=?"
+    result = cursor.execute(query,(link,logo, name))
+    connection.commit()
+    connection.close()
+
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
---
+++ create_tables.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+# make a connection
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+create_table = """CREATE TABLE IF NOT EXISTS manufacturers (
+                        name text,
+                        link text,
+                        logo text
+                    )"""
+cursor.execute(create_table)
+
+connection.commit()
+connection.close()
